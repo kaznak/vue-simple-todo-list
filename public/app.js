c36a809d1ef45dbc649b9748b4149732e2cc58eb
Kazutaka <kaznak.at.work@gmail.com>
2019-05-24 01:08:01 +0900

    data object

--- public/app.js
+++ public/app.js
@@ -1,33 +1,51 @@
 
+function Todo(text, isChecked) {
+    this.text = text
+    this.isChecked = isChecked
+    this.flipCheck = function() {
+        this.isChecked = ! this.isChecked
+        return this
+    }
+    return this
+}
+
+function TodoList(init) {
+    if(init == undefined) {
+        this.count = 0
+        this.todoList = []
+    } else {
+        this.count = init.length
+        this.todoList = init.map(function(e,i){
+            e.id = i
+            return e
+        })
+    }
+    this.addTodo = function(text, checked) {
+        this.count += 1
+        var todo = new Todo(text, checked)
+        todo.id = this.count
+        this.todoList.push(todo)
+        return this
+    }
+    this.cleanChecked = function() {
+        this.todoList = this.todoList.filter(todo => ! todo.isChecked)
+        return this
+    }
+    return this
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+var todoList = new TodoList([
+    new Todo("Pick up Paycheck", true),
+    new Todo("Cash Paycheck"),
+    new Todo("Get Milk"),
+])
+
 var app = new Vue({
     el: '#app',
     data: {
-        count : 3,
-        todos: [
-            { id : 0, isChecked : true,
-              text : "Pick up Paycheck" },
-            { id : 1, isChecked : false,
-              text : "Cash Paycheck" },
-            { id : 2, isChecked : false,
-              text : "Get Milk" }
-        ],
+        todoList : todoList,
         newTodoField : ""
     },
-    methods : {
-        addTodo : function() {
-            this.count += 1
-            this.todos.push({
-                id : this.count,
-                isChecked : false,
-                text : this.newTodoField
-            })
-            this.newTodoField = ""
-        },
-        flipCheck : function(item) {
-            item.isChecked = ! item.isChecked
-        },
-        cleanChecked : function() {
-            this.todos = this.todos.filter(todo => ! todo.isChecked)
-        }
-    }
 })
