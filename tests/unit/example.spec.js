e9a8c3950af4fe647e8e6db9306b7faabb18be58
Kazutaka <kaznak.at.work@gmail.com>
2019-05-28 00:58:53 +0900

    fix exception

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,11 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify';
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+})
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
