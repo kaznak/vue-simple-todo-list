911b6d2989b68277bbb98e781bccc673447af5dd
Kazutaka <kaznak.at.work@gmail.com>
2019-05-28 23:55:34 +0900

    add TodoApp

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,47 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <!-- <v-toolbar app> -->
+    <!--   <v-toolbar-title class="headline text-uppercase"> -->
+    <!--     <span>Vuetify</span> -->
+    <!--     <span class="font-weight-light">MATERIAL DESIGN</span> -->
+    <!--   </v-toolbar-title> -->
+    <!--   <v-spacer></v-spacer> -->
+    <!--   <v-btn -->
+    <!--     flat -->
+    <!--     href="https://github.com/vuetifyjs/vuetify/releases/latest" -->
+    <!--     target="_blank" -->
+    <!--   > -->
+    <!--     <span class="mr-2">Latest Release</span> -->
+    <!--   </v-btn> -->
+    <!-- </v-toolbar> -->
+
+    <!-- <v-content> -->
+    <!--   <HelloWorld msg="Welcome to Vuetify"/> -->
+    <!-- </v-content> -->
 
     <v-content>
-      <HelloWorld msg="Welcome to Vuetify"/>
+      <TodoApp title="Todo List"
+	       v-bind:todo-list="todoList"
+	       placeholder="Enter todo and press enter. "/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoApp from './components/TodoApp'
+import IdArray from './lib/IdArray'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
+    name: 'App',
+    components: {
+ TodoApp
+    },
+    data : () => ({
+todoList : new IdArray([
+{ text : "Pick up Paycheck", isChecked : true },
+{ text : "Cash Paycheck", isChecked : false },
+{ text : "Get Milk", isChecked : false },
+])
+})
 }
 </script>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <h1>
+          {{ title }}
+        </h1>
+      </v-flex>
+      <v-flex>
+ <ul>
+     <li v-for="item in todoList.array" v-bind:key="item.id"
+  v-on:click="item.isChecked = !item.isChecked">
+       <p v-bind:class="{ done: item.isChecked }">{{ item.text }}</p>
+     </li>
+ </ul>
+ <input type="text"
+        v-bind:placeholder="placeholder"
+        v-model="newTodoField"
+        v-on:keyup.enter="todoList.push({ text : newTodoField, isChecked : false });newTodoField = '';" />
+ <button v-on:click="todoList.filter(x => ! x.isChecked)">
+   delete checked
+ </button>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+    props: [
+"title",
+"todoList", // lib/IdArray
+"placeholder",
+    ],
+    data: () => ({
+ newTodoField : ""
+    })
+}
+</script>
+
+<style>
+.done { text-decoration: line-through; }
+</style>
