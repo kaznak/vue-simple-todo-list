43cc87e50a657d2684f06b5cb1d9c9eb53a3f818
Kazutaka <kaznak.at.work@gmail.com>
2019-05-29 00:53:06 +0900

    Use vuetify components

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,25 +1,34 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-        <h1>
-          {{ title }}
-        </h1>
-      </v-flex>
-      <v-flex>
- <ul>
-     <li v-for="item in todoList.array" v-bind:key="item.id"
-  v-on:click="item.isChecked = !item.isChecked">
-       <p v-bind:class="{ done: item.isChecked }">{{ item.text }}</p>
-     </li>
- </ul>
- <input type="text"
-        v-bind:placeholder="placeholder"
-        v-model="newTodoField"
-        v-on:keyup.enter="todoList.push({ text : newTodoField, isChecked : false });newTodoField = '';" />
- <button v-on:click="todoList.filter(x => ! x.isChecked)">
-   delete checked
- </button>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+	<v-toolbar>
+	  <v-toolbar-title>
+            {{ title }}
+	  </v-toolbar-title>
+	</v-toolbar>
+	<v-list>
+	    <v-list-tile
+	      v-for="item in todoList.array"
+	      v-bind:class="{ done: item.isChecked }"
+              v-bind:key="item.id"
+	      v-on:click="item.isChecked = !item.isChecked">
+	      {{ item.text }}
+	    </v-list-tile>
+	</v-list>
+	<v-toolbar>
+	  <v-text-field
+	    v-model="newTodoField"
+	    v-bind:label="placeholder"
+	    v-on:keyup.enter="todoList.push({ text : newTodoField, isChecked : false }); newTodoField = '';"
+	    required >
+	  </v-text-field>
+	  <v-spacer></v-spacer>
+	  <v-btn
+	    v-on:click="todoList.filter(x => ! x.isChecked)">
+	    delete checked
+	  </v-btn>
+	</v-toolbar>
       </v-flex>
     </v-layout>
   </v-container>
