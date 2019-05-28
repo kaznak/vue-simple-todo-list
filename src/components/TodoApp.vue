5f4ab23ef1c0fdd1e54e48f47030e27ce33bf770
Kazutaka <kaznak.at.work@gmail.com>
2019-05-29 01:36:49 +0900

    add Todo when input is not empty

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,49 +1,62 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
-	<v-toolbar>
-	  <v-toolbar-title>
-            {{ title }}
-	  </v-toolbar-title>
-	</v-toolbar>
-	<v-list>
-	    <v-list-tile
-	      v-for="item in todoList.array"
-	      v-bind:class="{ done: item.isChecked }"
-              v-bind:key="item.id"
-	      v-on:click="item.isChecked = !item.isChecked">
-	      {{ item.text }}
-	    </v-list-tile>
-	</v-list>
-	<v-toolbar>
-	  <v-text-field
-	    v-model="newTodoField"
-	    v-bind:label="placeholder"
-	    v-on:keyup.enter="todoList.push({ text : newTodoField, isChecked : false }); newTodoField = '';"
-	    required >
-	  </v-text-field>
-	  <v-spacer></v-spacer>
-	  <v-btn
-	    v-on:click="todoList.filter(x => ! x.isChecked)">
-	    delete checked
-	  </v-btn>
-	</v-toolbar>
-      </v-flex>
-    </v-layout>
-  </v-container>
+<v-container>
+  <v-layout>
+    <v-flex>
+      <v-toolbar>
+	<v-toolbar-title>
+          {{ title }}
+	</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+	<v-list-tile
+	  v-for="item in todoList.array"
+	  v-on:click="item.isChecked = !item.isChecked"
+          v-bind:key="item.id"
+	  >
+	  <span v-bind:class="{ done: item.isChecked }">
+	    {{ item.text }}
+	  </span>
+	</v-list-tile>
+      </v-list>
+      <v-toolbar>
+	<v-text-field
+	  v-model="newTodoField"
+	  v-bind:label="placeholder"
+	  v-on:keyup.enter="pushTodo(newTodoField)"
+	  required >
+	</v-text-field>
+	<v-spacer></v-spacer>
+	<v-btn
+	  v-on:click="todoList.filter(x => ! x.isChecked)">
+	  delete checked
+	</v-btn>
+      </v-toolbar>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
 export default {
     props: [
-"title",
-"todoList", // lib/IdArray
-"placeholder",
+	"title",
+	"todoList", // lib/IdArray
+	"placeholder",
     ],
     data: () => ({
- newTodoField : ""
-    })
+	newTodoField : ""
+    }),
+    methods : {
+	pushTodo : function(text) {
+	    if(this.newTodoField.trim()!=''){
+		this.todoList.push({
+		    text : this.newTodoField,
+		    isChecked : false
+		})
+		this.newTodoField = ''
+	    }
+	}
+    }
 }
 </script>
 
