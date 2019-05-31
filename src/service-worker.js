377cbce6e5897bb053500f842dce4d3cc856ea27
Kazutaka <kaznak.at.work@gmail.com>
2019-05-31 17:00:37 +0900

    update service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,20 @@
+/* eslint-disable no-console */
+
+'use strict';
 
 self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+
 });
